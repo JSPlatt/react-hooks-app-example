a6e5bd812350dc324ebfe2b55ca29b0e79dbb0fc
Jeffrey Platt <JSPlatt@gmail.com>
2021-04-03 14:20:02 -0400

    commit tree 3680951fffe3de702e9d762ccb5f782db1d0c1b3

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 import TestComponent from "./TestComponent";
 
+// Add your code own within the return statement
 function App() {
   return (
     <div className="App">
